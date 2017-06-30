9d15a821a12f59f8fd1c459affaf051e8e94786c
lewis nyoike <nyoikelewis@gmail.com>
2017-06-30 12:46:56 +0300

    Set the app to fetch input of the word from the user, and rendered  the fetched word to the instance variable to in order to pass it as an argument in the method I defined in the rb file

--- app.rb
+++ app.rb
@@ -3,3 +3,10 @@ require('sinatra')
   require('./lib/scrabble_score')
   require('descriptive_statistics')
   also_reload('lib/**/*.rb')
+
+
+  get('/') do
+    erb(:form)
+    word = params.fetch('word')
+    @content = params.fetch('content').count(word)
+  end
